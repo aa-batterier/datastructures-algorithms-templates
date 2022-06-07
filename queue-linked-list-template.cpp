b2f013dd6b63e57c6d3b55922c6421dd817bf15a
Andreas Johansson <aj@outlook.com>
2022-06-07 15:57:57 +0200

    Added first version of the queue template implemented with a linked list.

---
+++ queue-linked-list-template.cpp
@@ -0,0 +1,101 @@
+#include <assert.h>
+
+/*
+ * Queue (FIFO, First In First Out) example in C++.
+ * Implemented through a linked list.
+ */
+
+class Queue
+{
+
+        private:
+                struct Link
+                {
+                        // Constructor for the struct.
+                        Link(const double data, Link* pNext) : _data(data), _pNext(pNext) {}
+                        double _data;
+                        Link *_pNext;
+                };
+                int _size;
+                Link *_pBegining, *_pEnd;
+
+        public:
+                Queue() // Constructor for the class.
+                {
+                        _pBegining = nullptr;
+                        _pEnd = nullptr;
+                        _size = 0;
+                }
+
+                Queue(const Queue& other) // Copies the object.
+                {
+                        _pBegining = nullptr;
+                        _pEnd = nullptr;
+                        _size = 0;
+                        while (_size < other.getSize())
+                        {
+                                push(other.getData());
+                        }
+                }
+
+                ~Queue() // Destructor.
+                {
+                        while (_size > 0)
+                        {
+                                pop();
+                        }
+                        _pBegining = nullptr;
+                        _pEnd = nullptr;
+                        _size = 0;
+                }
+
+                void push(const double data)
+                {
+                        Link *pNew = new Link(data, nullptr);
+                        (*_pEnd)._pNext = pNew;
+                        _pEnd = pNew;
+                        _size++;
+                }
+
+                void pop(void)
+                {
+                        assert(_size > 0);
+                        Link *pDelete = _pBegining;
+                        _pBegining = (*pDelete)._pNext;
+                        delete pDelete;
+                        _size--;
+                }
+
+                void insert(Link *pBefore, const double data)
+                {
+                        (*pBefore)._pNext = new Link(data, (*pBefore)._pNext);
+                }
+
+                void eraseAfter(Link *pBefore)
+                {
+                        assert(_size > 0);
+                        Link *pDelete = (*pBefore)._pNext;
+                        (*pBefore)._pNext = (*pDelete)._pNext;
+                        delete pDelete;
+                }
+
+                Link* getBegining(void)
+                {
+                        return _pBegining;
+                }
+
+                Link* getEnd(void)
+                {
+                        return _pEnd;
+                }
+
+                double& getData(void)
+                {
+                        return (*_pEnd)._data;
+                }
+
+                const int getSize(void)
+                {
+                        return _size;
+                }
+};
