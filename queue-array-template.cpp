84bd801a0d44649df2dc09876b30ce7158ae1ebd
Andreas Johansson <aj@outlook.com>
2022-06-08 14:54:52 +0200

    Improved on the queue template implemented with an array.

--- queue-array-template.cpp
+++ queue-array-template.cpp
@@ -8,7 +8,7 @@
 class Stack
 {
         private:
-                int _capacity, _size, _begining, _end;
+                int _capacity, _size, _begining;
                 double *_pArr;
 
                 void copyArray(const double* source, double* destination, const int size)
@@ -19,19 +19,13 @@ class Stack
                         }
                 }
 
-                const int calculateEnd(const int begining, const int size, const int capacity)
-                {
-                        return (begining + size) % capacity;
-                }
-
         public:
                 Stack(void)
                 {
-                        _capacity = 1000;
+                        _capacity = 1;
                         _size = 0;
                         _begining = 0;
-                        _end = calaculateEnd(_begining, _size, _capacity);
-                        _pArr = new double[_capacity];
+                        _pArr = new double [_capacity];
                 }
 
                 Stack(const Stack& other)
@@ -39,42 +33,47 @@ class Stack
                         _capacity = other.getCapacity();
                         _size = other.getSize();
                         _begining = other.getBegining();
-                        _end = other.getEnd();
                         _pArr = new double[_capacity];
                         copyArray(other.getArr(),_pArr,_size);
                 }
 
                 ~Stack()
                 {
-                        delete _pArr;
-                        _size = 0;
-                        _capacity = 1000;
+                        delete [] _pArr;
                 }
 
                 void push(const double data)
                 {
                         if (_size >= _capacity)
                         {
-                                double *newArr = new double[_capacity*2];
+                                int newCapacity = _capacity * 2;
+                                double *newArr = new double [newCapacity];
                                 copyArray(_pArr,newArr,_capacity);
-                                delete _pArr;
+                                _capacity = newCapacity;
+                                delete [] _pArr;
                                 _pArr = newArr;
-                                _capacity =* 2;
                         }
-                        _end = calculateEnd(_begining,_size,_capacity);
-                        _pArr[_end] = data;
+                        _pArr[(_begining + _size) % _capacity] = data;
                         _size++;
                 }
 
                 void pop(void)
                 {
                         assert(_size > 0);
+                        _begining = (_begining + 1) % _capacity;
                         _size--;
                 }
 
-                double& getTop(void)
+                double& getBegining(void)
                 {
-                        return _pArr[_end];
+                        assert(_size > 0);
+                        return _pArr[_begining];
+                }
+
+                double& getEnd(void)
+                {
+                        assert(_size > 0);
+                        return _pArr[(_begining + _size - 1) % _capacity];
                 }
 
                 double* getArr(void)
